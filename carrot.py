5755afc20b16a75cb2e608e444b41b5a27f680d3
ra314 <55324331+ra314@users.noreply.github.com>
2020-09-03 21:52:37 +1000

    Gui version of carrot.
    Done with kivy and has many of the features from the original script implemented.

--- carrot.py
+++ carrot.py
@@ -1,10 +1,11 @@
-#Libs
+### Backend
+
 import pandas as pd
 from datetime import datetime
 from datetime import timedelta
+from columnar import columnar
 
-#Creating the empty df
-#df = pd.DataFrame(columns = ['Date', 'Category', 'Description', 'Carrot'])
+df = pd.read_csv('carrots.csv')
 
 #Adding Tasks
 def add_task(df, category, description, carrot):
@@ -13,75 +14,134 @@ def add_task(df, category, description, carrot):
 	print(f"\nYou've earned {carrot}XP")
 	df.to_csv('carrots.csv', index = False)
 
-#Sample Usage
-#add_task(df, 'Hygiene', 'Brushed Teeth', 1)
-
-#Exporting and Importing
-#df.to_csv('carrots.csv', index = False)
-df = pd.read_csv('carrots.csv')
-
-#Todo List
-#Calculate XP without certain categories
-#Handle Error for when there are no tasks done on a certain day
-
 #Function to calculate XP gained in a day
-def xp_per_day(df, selected_date_str):
+def xp_per_day(df, selected_date_str, restricted_categories):
 	xp = 0
 	selected_date = datetime.strptime(selected_date_str, '%d-%m-%Y')
 	for row in df.iterrows():
 		curr_date_str = row[1]['Date']
+		curr_category = row[1]['Category']
 		curr_date = datetime.strptime(curr_date_str, '%d-%m-%Y %H:%M:%S')
 		if curr_date.date() == selected_date.date():
-			xp += row[1]['Carrot']
+			if (curr_category not in restricted_categories):
+				xp += row[1]['Carrot']
 	return xp
 
-#Sample Usage
-#xp_per_day(df, '16-08-2020')
-
-#Function to edit xp
-def edit_xp(df, index, new_xp):
-	df.at[index, 'Carrot'] = new_xp
-	print("\nValue has been updated.")
-	df.to_csv('carrots.csv', index = False)
-
-#Sample Usage
-#edit_xp(df, 15, 15)
-
 #Function to calculate XP gained in last x days
-def xp_in_last_x_days(df, x):
-	headers = ['XP', 'Date']
+def xp_in_last_x_days(df, x, restricted_categories):
+	headers = ['Date', 'Day', 'XP']
 	data = []
 	total_xp = 0
+
 	today_str = datetime.now().strftime('%d-%m-%Y')
 	today = datetime.strptime(today_str, '%d-%m-%Y')
+
 	for i in range(x-1,-1,-1):
 		date = (today-timedelta(days=i)).strftime('%d-%m-%Y')
-		xp = xp_per_day(df, date)
+		xp = xp_per_day(df, date, restricted_categories)
 		total_xp += xp;
-		data.append([str(xp), date])
+		day = datetime.strptime(date, '%d-%m-%Y')
+		data.append([date, day.strftime('%A'), str(xp)])
+
 	table = columnar(data, headers)
 	print(table)
+
 	print(f"Fuck yeah you earned {total_xp}XP in the last {x} days!!!")
 
-#Sample Usage
-#xp_in_last_x_days(df, 7)
-
 #Function to print tasks done on a certain day
-def tasks_on_day(df, selected_date_str):
-    selected_date = datetime.strptime(selected_date_str, '%d-%m-%Y')
-    headers = ['Category', 'Description', 'Carrot']
-    data = []
-    for row in df.iterrows():
-    	curr_date_str = row[1]['Date']
-    	curr_date = datetime.strptime(curr_date_str, '%d-%m-%Y %H:%M:%S')
-    	if curr_date.date() == selected_date.date():
-    		data.append([
-                row[1][headers[0]],
-                row[1][headers[1]],
-                str(row[1][headers[2]])
-            ])
-    table = columnar(data, headers, terminal_width=100)
-    print(table)
+def print_tasks_on_day(df, selected_date_str, restricted_categories):
+	selected_date = datetime.strptime(selected_date_str, '%d-%m-%Y')
+	headers = ['Category', 'Description', 'Carrot']
+	data = []
+	total_xp = 0
 
-#Sample Usage
-#tasks_on_day(df, '28-08-2020')
+	for row in df.iterrows():
+		curr_date_str = row[1]['Date']
+		curr_date = datetime.strptime(curr_date_str, '%d-%m-%Y %H:%M:%S')
+
+		if curr_date.date() == selected_date.date():
+			if row[1]['Category'] not in restricted_categories:
+				data.append([
+				row[1][headers[0]],
+				row[1][headers[1]],
+				str(row[1][headers[2]])
+				])
+				total_xp += row[1]['Carrot']
+
+	table = columnar(data, headers, terminal_width=100)
+	print("On " + selected_date_str + " you earned " + str(total_xp) + "XP")
+	print(table)
+
+### TODO:
+#Implement dropdown table for category
+#Add option to search for previous Tasks
+#Add drowdown option for common tasks
+
+### Front end
+
+import kivy
+
+from kivy.app import App
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button
+from kivy.uix.dropdown  import DropDown
+
+from kivy.core.window import Window
+Window.size = (900, 900)
+
+# This class stores the info of .kv file
+# when it is called goes to my.kv file
+class MainWidget(GridLayout):
+	def print_tasks_on_day(self):
+		date = self.ids.input_date.text
+		categories = self.ids.input_blacklist_categories.text
+		restricted_categories = categories.replace(" ","").split(",")
+
+		if date == "":
+			date = datetime.now().strftime('%d-%m-%Y')
+
+		print_tasks_on_day(df, date, restricted_categories)
+
+		self.ids.input_date.text = ""
+		self.ids.input_blacklist_categories.text = ""
+
+	def xp_in_last_x_days(self):
+		categories = self.ids.input_blacklist_categories.text
+		restricted_categories = categories.replace(" ","").split(",")
+		num_days = self.ids.input_num_days.text
+
+		if num_days == "":
+			num_days = 7
+		else:
+			num_days = int(num_days)
+
+		xp_in_last_x_days(df, num_days, restricted_categories)
+
+		self.ids.input_blacklist_categories.text = ""
+		self.ids.input_num_days.text = ""
+
+	def add_task(self):
+		description = self.ids.input_description.text
+		category = self.ids.input_category.text
+		carrots = int(self.ids.input_carrots.text)
+
+		add_task(df, category, description, carrots)
+
+		self.ids.input_description.text = ""
+		self.ids.input_category.text = ""
+		self.ids.input_carrots.text = ""
+
+	pass
+
+# we are defining the Base Class of our Kivy App
+class carrotApp(App):
+	def build(self):
+		# return a MainWidget() as a root widget
+		return MainWidget()
+
+if __name__ == '__main__':
+
+    # Here the class MyApp is initialized
+    # and its run() method called.
+    carrotApp().run()
